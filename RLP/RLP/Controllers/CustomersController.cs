d894eab606e302d596d7ef69bfd941a9b9de540d
Ziborn <robin.mik23@gmail.com>
2017-02-27 08:58:55 +0100

    Add files via upload

---
+++ RLP/RLP/Controllers/CustomersController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RLP.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RLP.Controllers
+{
+    public class CustomersController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var models = DataManager.GetAllCustomers();
+            ViewBag.Customers = models;
+            return View();
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            DataManager.AddCustomer(customer);
+            return RedirectToAction(nameof(CustomersController.Index));
+        }
+
+        public IActionResult Details(int id)
+        {
+            var models = DataManager.GetCustomerById(id);
+            ViewBag.Customers = models;
+            return View();
+        }
+
+        public IActionResult Remove(Customer customer)
+        {
+            DataManager.Remove(customer);
+            return View();
+        }
+    }
+}
---
+++ RLP/RLP/Models/DataManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RLP.Models;
+
+namespace RLP.Models
+{
+    public class DataManager
+    {
+        private static List<Customer> customers = new List<Customer>
+        {
+            new Customer {Id = 1, CompanyName = "Robins grönsaker", EmployeeCount = 3, Address = "Vägen 1"},
+            new Customer {Id = 2, CompanyName = "Pascals skate", EmployeeCount = 1, Address = "Gatan 2"},
+            new Customer {Id = 3, CompanyName = "Love kök och bar", EmployeeCount = 100, Address = "Allén 3"},
+        };
+        
+        public static void AddCustomer(Customer customer)
+        {
+            customer.Id = customers.Max(c => c.Id) + 1;
+            customers.Add(customer);
+        }
+
+        public static Customer[] GetAllCustomers()
+        {
+            return customers.ToArray();
+        }
+
+        public static IEnumerable <Customer> GetCustomerById(int id)
+        {
+            return customers.Where(c => c.Id == id);
+        }
+
+        public static void Remove(Customer customer)
+        {
+            int i = customer.Id;
+            customers.RemoveAt(i);
+        }
+    }
+}
